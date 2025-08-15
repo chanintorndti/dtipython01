c22b0f22e3dbfade0111d3ba288d1809e0686857
chanintorndti <chanintorn.dti@gmail.com>
2025-08-15 16:06:55 +0700

    V.2: code ws01-04 but 05 not complete

--- workshop01.py
+++ workshop01.py
@@ -1,1 +1,18 @@
-print('Hi...DTI-SAU')
+# Comment คือ อะไรที่ Dev พิมพ์ปนไปใน Source Code แต่ไม่มีผลต่อการทำงาน
+# Python - Comment มี 2 ประเภท 3่ แบบ
+# 1. Single Line Comment เขียนอยู่หลังเครื่องหมาย #
+# Wow wow wow
+print('Hello....')
+
+# 2. Multi Line Comment เขียนอยู่ระหว่างเครื่องหมาย '''?????''', """?????"""
+''' (triple single quote)
+    asdfasdf
+    asdfasdfasdf
+    asdfasdfasdfasdfadsf
+'''
+
+"""(triple double quote)
+    asdfasdf
+    adsfadsfadsf
+    adsfasdfasdfasdfasdf
+"""
---
+++ workshop03.py
@@ -0,0 +1,19 @@
+# คำสั่งแสดงผลข้อมูลใดๆ ที่หน้าจอ ใช้ฟังก์ชัน print()
+# string ข้อความ
+print('Hello')
+print("Hi")
+print('''Hey''')
+print("""Hum""")
+
+# number ตัวเลข
+print(48654)    #integer/int
+print(15.4568)  #float
+
+# boolean ตรรกะ มีแค่ 2 ค่า True, False
+print(True)
+print(False)
+
+# expression นิพจน์ คือ อะไรก็ตามที่มี process การทำงาน เช่น การคำนวณ, การเปรียบเทียบ, ตัวแปร, ....
+print(10 + 20)
+print(50 > 90)
+print('Bangna' <= 'Bangpoo')
